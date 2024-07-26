17962736e8e9d1844207b2f11edda84920439297
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2024-07-26 22:43:53 +0530

    bg changer

---
+++ 04bgchanger/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+function App() {
+  const [color, setColor] = useState("gray")
+
+  return (
+    <div className="w-full h-screen duration-200"
+    style={{backgroundColor: color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-2 shadow-lg bg-white px-3 py-2 rounded-lg'>
+          <button
+          onClick={() => {setColor("red")}}
+           className='outline-none px-4 py-2 rounded-full text-white shadow-lg'
+          style={{backgroundColor: "red"}}>Red</button>
+          <button 
+          onClick={() => {setColor("green")}}
+          className='outline-none px-4 py-2 rounded-full text-white shadow-lg'
+          style={{backgroundColor: "green"}}>Green</button>
+          <button 
+          onClick={() => {setColor("aqua")}}
+          className='outline-none px-4 py-2 rounded-full text-black shadow-lg'
+          style={{backgroundColor: "aqua"}}>Aqua</button>
+          <button
+          onClick={() => {setColor("blue")}}
+          className='outline-none px-4 py-2 rounded-full text-white shadow-lg'
+          style={{backgroundColor: "blue"}}>Blue</button>
+          <button
+          onClick={() => {setColor("black")}}
+          className='outline-none px-4 py-2 rounded-full text-white shadow-lg'
+          style={{backgroundColor: "black"}}>Black</button>
+          <button
+          onClick={() => {setColor("pink")}}
+          className='outline-none px-4 py-2 rounded-full text-black shadow-lg'
+          style={{backgroundColor: "Pink"}}>Pink</button>
+          <button
+          onClick={() => {setColor("yellow")}}
+          className='outline-none px-4 py-2 rounded-full text-black shadow-lg'
+          style={{backgroundColor: "yellow"}}>Yellow</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
