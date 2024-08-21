640a2ef83b0d19dd12f08468c24f4445b08c254d
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2024-08-21 22:10:55 +0530

    Portfolio

--- Portfolio/src/Components/Project.jsx
+++ Portfolio/src/Components/Project.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import calculator from "../assets/calculator.jpg"
+import password from "../assets/Password.jpeg" 
 
 function Project() {
   return (
@@ -7,9 +9,41 @@ function Project() {
           <div className='container mx-auto px-8 md:px-16 lg:px-24'>
             <h2 className='text-4xl font-bold text-center mb-12 bg-gradient-to-l  from-pink-400 to-yellow-500 text-transparent bg-clip-text'>My Projects</h2>
             <div className='grid grid-col-1 md:grid-cols-2 lg:grid-cols-3'>
-              <div className='bg-gray-800 rounded-lg px-6 pb-6 hover: shadow-lg transform transition-transform hover:scale-105 duration-300'>
-                <img src="" alt="" />
-              </div>
+              <span className='bg-gray-800 rounded-lg px-6 pb-6 hover: shadow-lg transform transition-transform hover:scale-105 duration-300 m-3'>
+                <img src={calculator} alt="" />
+                <h2 className='text-center font-bold text-2xl my-4 bg-gradient-to-r from-green-400 to-blue-500 text-transparent bg-clip-text'>Simple Calculator</h2>
+                  <div className='px-4 my-3'>
+                    <h3>Using Skills-</h3>
+                    <p>HTML, CSS, JavaScript</p>
+                  </div>
+                  <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-5 py-2 mx-3 mt-3 rounded-full border-black'>
+                    <a href="https://github.com/Ujjwal81011/Minor-Project/blob/main/calculator.html" target='_blank'>Git Hub</a>
+                  </button>
+              </span>
+              
+              <span className='bg-gray-800 rounded-lg px-6 pb-6 hover: shadow-lg transform transition-transform hover:scale-105 duration-300 m-3'>
+                <img src={password} alt="" className='rounded-lg' />
+                <h2 className='text-center font-bold text-2xl my-4 bg-gradient-to-r from-green-400 to-blue-500 text-transparent bg-clip-text'>Password Generator</h2>
+                  <div className='px-4 my-3'>
+                    <h3>Using Skills-</h3>
+                    <p>HTML, CSS, JavaScript, React.js</p>
+                  </div>
+                  <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-5 py-2 mx-3 mt-3 rounded-full border-black'>
+                    <a href="https://github.com/Ujjwal81011/Minor-Project/blob/main/calculator.html" target='_blank'>Git Hub</a>
+                  </button>
+              </span>
+
+              <span className='bg-gray-800 rounded-lg px-6 pb-6 hover: shadow-lg transform transition-transform hover:scale-105 duration-300 m-3'>
+                <img src={calculator} alt="" />
+                <h2 className='text-center font-bold text-2xl my-4 bg-gradient-to-r from-green-400 to-blue-500 text-transparent bg-clip-text'>Simple Calculator</h2>
+                  <div className='px-4 my-3'>
+                    <h3>Using Skills-</h3>
+                    <p>HTML, CSS, JavaScript</p>
+                  </div>
+                  <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-5 py-2 mx-3 mt-3 rounded-full border-black'>
+                    <a href="https://github.com/Ujjwal81011/Minor-Project/blob/main/calculator.html" target='_blank'>Git Hub</a>
+                  </button>
+              </span>
             </div>
           </div>
         </div>
