17ac314c0f3fd9268405f015da857a412e050978
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2024-08-21 21:04:17 +0530

    Portfolio

--- Portfolio/src/Components/Project.jsx
+++ Portfolio/src/Components/Project.jsx
@@ -7,7 +7,9 @@ function Project() {
           <div className='container mx-auto px-8 md:px-16 lg:px-24'>
             <h2 className='text-4xl font-bold text-center mb-12 bg-gradient-to-l  from-pink-400 to-yellow-500 text-transparent bg-clip-text'>My Projects</h2>
             <div className='grid grid-col-1 md:grid-cols-2 lg:grid-cols-3'>
-
+              <div className='bg-gray-800 rounded-lg px-6 pb-6 hover: shadow-lg transform transition-transform hover:scale-105 duration-300'>
+                <img src="" alt="" />
+              </div>
             </div>
           </div>
         </div>
