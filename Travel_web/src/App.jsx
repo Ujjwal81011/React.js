fddc8cff769adb44726083c101b406ae72d295e8
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2024-10-01 23:43:51 +0530

    let me check

--- Travel_web/src/App.jsx
+++ Travel_web/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
       <h1 className='bg-black font-bold text-white text-center'>Ujjwal Mishra....</h1>
+      <p className='text-2xl text-yellow-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ad!</p>
     </>
   )
 }
