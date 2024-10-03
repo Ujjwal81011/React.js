eb995489023f4022c550bce53f6a8cc8d3fbe17f
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2024-10-03 21:58:43 +0530

    Portfolio

--- Portfolio/src/Components/Navbar.jsx
+++ Portfolio/src/Components/Navbar.jsx
@@ -12,7 +12,9 @@ function Navbar() {
                 <a href="#education" className='hover:text-gray-400'>Education</a>
                 <a href="#contact" className='hover:text-gray-400'>Contact</a>
             </div>
-            <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Connect Me</button>
+            <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>
+              <a href="https://www.linkedin.com/in/ujjwal-mishra-782634277/" target='_blank'>Connect Me</a>
+            </button>
         </div>
     </nav>
   )
