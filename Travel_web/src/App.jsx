d166dcf906eadad61508f14a5a25da8b72d7e562
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2024-10-08 23:32:46 +0530

    Travel

--- Travel_web/src/App.jsx
+++ Travel_web/src/App.jsx
@@ -4,8 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-black font-bold text-white text-center'>Ujjwal Mishra....</h1>
-      <p className='text-2xl text-yellow-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ad!</p>
+      
     </>
   )
 }
