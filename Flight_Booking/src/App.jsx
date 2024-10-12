ea6ef6b17fc6795a3ebc510c7edd33d0b6ca30ac
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2024-10-12 21:14:58 +0530

    flight

---
+++ Flight_Booking/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Topbar from './Components/Topbar'
+
+function App() {
+
+  return (
+    <>
+      <Topbar />
+    </>
+  )
+}
+
+export default App
---
+++ Flight_Booking/src/Components/Topbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {  FaCaretSquareRight} from "react-icons/fa";
+
+function Topbar() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Topbar
