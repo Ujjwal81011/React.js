c7fbb76a65aa36e435231f26b3bbef6b14e757de
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2024-10-13 00:26:20 +0530

    flight

---
+++ Flight_Booking/src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { SiConsul } from "react-icons/si";
+import { BsPhoneVibrate } from "react-icons/bs";
+import { AiOutlineGlobal } from "react-icons/ai";
+
+function Navbar() {
+  return (
+    <div className='bg-slate-200'>
+    <div className='flex justify-between items-center sm:mx-6  lg:mx-12 py-3'>
+        <div><SiConsul/></div>
+        <div className='flex gap-4'>
+            <li className='flex'> <BsPhoneVibrate className='mt-1 mx-2'/> Support</li>
+            <li className='flex'> <AiOutlineGlobal className='mt-1 mx-2'/> Languages</li>
+        </div>
+
+        <div className='flex'>
+            <button className='rounded-2xl w-14 hover:font-bold'>Sign In</button>
+        </div>
+    </div>
+    </div>
+
+  )
+}
+
+export default Navbar
