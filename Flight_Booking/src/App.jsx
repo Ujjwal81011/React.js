f30e662ef06d83d10698a8c38e9b49ffc6a9eb36
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2024-10-17 23:10:10 +0530

    Flight

--- Flight_Booking/src/App.jsx
+++ Flight_Booking/src/App.jsx
@@ -2,17 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Topbar from "./Components/Topbar";
 import Home from "./Components/Home";
-import Error404 from "./Components/Error404"; 
 
 function App() {
   return (
     <>
       <Navbar />
       <Topbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<Error404 />} />
-      </Routes>
+      <Home />
     </>
   );
 }
