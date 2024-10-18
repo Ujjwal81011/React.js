2dd1680470c79f071268f689df18ddc785a5f7a2
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2024-10-18 22:58:26 +0530

    Features

--- Flight_Booking/src/Components/Features.jsx
+++ Flight_Booking/src/Components/Features.jsx
@@ -5,9 +5,12 @@ export default function Features() {
     <div className='flex w-full md:px-[200px] px-8 flex-col gap-16 justify-center items-center'>
         <div className='flex flex-col w-full items-center'>
             <span className='text font-semibold text-sky-800 mt-4'>Our Features</span>
-            <span className='text-3xl font-semibold mt-2'>
+            <span className='md:text-3xl text-2xl font-semibold mt-2'>
                 Our Priceless Features
             </span>
+            <p className='text-center mt-2 max-w-[400px]'>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis molestiae, eius adipisci nam tenetur vel.
+            </p>
         </div>
     </div>
   )
