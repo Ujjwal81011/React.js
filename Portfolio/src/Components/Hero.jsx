5716039f37d138504333095036a40def37358cbf
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2025-02-26 23:09:41 +0530

    Portfolio

--- Portfolio/src/Components/Hero.jsx
+++ Portfolio/src/Components/Hero.jsx
@@ -19,7 +19,7 @@ function Hero() {
             <a href="#contact">Contact with Me</a>
             </button>
             <button className='bg-gradient-to-r from-pink-400 to-yellow-500 text-white md:inline ttransform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>
-              <a href="https://drive.google.com/file/d/1g79w62rTxj-7rCTtU0asHa3ZCvIEui2y/view?usp=drive_link" target='_blank'>Resume</a>
+              <a href="https://drive.google.com/file/d/1BERLlJkXeexDDv5cxb0-FvLK4P-g_QMm/view?usp=drive_link" target='_blank'>Resume</a>
           </button>
         </div>
     </div>
